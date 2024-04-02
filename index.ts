18e91a5a83eb75de0ffd5c3972d3d82e096ef0f2
hibasheikh90 <hayasheikh109@gmail.com>
2024-04-02 14:43:25 +0500

    Adding a TODO List Application

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todoList = [];
+let conditions = true;
+
+console.log("\n \t Welcome to Hibasheikh Todo List Application\n");
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your New Task:"
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask} Task added in Todo list successfully`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want more add task ?",
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore
+}
+console.log("Your Udated Todo List:", todoList);
